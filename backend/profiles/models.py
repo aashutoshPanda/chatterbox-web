66e2f7589dc92575c72a52dc3b084c416021eac9
aashutoshPanda <iamashutoshpanda@gmail.com>
2020-09-11 12:39:19 +0530

    fixed merge issues

---
+++ backend/profiles/models.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.CharField(max_length=1000, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    try:
+        instance.profile.save()
+    except:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
