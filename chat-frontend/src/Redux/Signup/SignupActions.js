7007666fd0761560f9670ed022e266316aebaf18
aashutoshPanda <iamashutoshpanda@gmail.com>
2020-09-15 14:39:52 +0530

    fixed signup request bug

--- chat-frontend/src/Redux/Signup/SignupActions.js
+++ chat-frontend/src/Redux/Signup/SignupActions.js
@@ -1,23 +1,23 @@
-export const userPostFetch = user => {
-    return dispatch => {
-      return fetch("http://localhost:8000/profile/auth/register", {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-        body: JSON.stringify({user})
-      })
-        .then(resp => resp.json())
-        .then(data => {
-            console.log(data)
-            localStorage.setItem("token", data.auth_token)
-            dispatch(loginUser(data.user_name))
-        })
-    }
-  }
-  
-  const loginUser = userObj => ({
-      type: 'LOGIN_USER',
-      payload: userObj
-  })
+export const userPostFetch = (user) => {
+  return (dispatch) => {
+    console.log("req object", user);
+    return fetch("http://localhost:8000/profile/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        localStorage.setItem("token", data.auth_token);
+        dispatch(loginUser(data.user_name));
+      });
+  };
+};
+
+const loginUser = (userObj) => ({
+  type: "LOGIN_USER",
+  payload: userObj,
+});
