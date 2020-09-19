c2a451a188085f23ab97db6e4fd8ba2dbc070617
aashutoshPanda <iamashutoshpanda@gmail.com>
2020-09-19 18:50:57 +0530

    fixed recursive issue in userlist

--- chat-frontend/src/Components/UserList.js
+++ chat-frontend/src/Components/UserList.js
@@ -1,39 +1,48 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {FetchAllUsers} from '../Redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { FetchAllUsers } from "../Redux";
 
 class UserList extends Component {
-
-  getUsers = () => this.props.FetchAllUsers()
-
-  handleClick=(item)=>{
+  handleClick = (item) => {
     // this.props.sendUser(item);
+  };
+  componentDidMount() {
+    this.props.FetchAllUsers();
   }
-
-  createTask=(item)=>{
-  return <div><p>{item.first_name} {item.last_name}</p> <button key={this.props.allUsers.username} onClick={()=>this.handleClick(item)}>Add Friend</button></div>
-  }
+  createTask = (item) => {
+    return (
+      <div>
+        <p>
+          {item.first_name} {item.last_name}
+        </p>{" "}
+        <button
+          key={this.props.allUsers.username}
+          onClick={() => this.handleClick(item)}
+        >
+          Add Friend
+        </button>
+      </div>
+    );
+  };
 
   render() {
-    this.getUsers(this.props.allUsers)
-    // console.log("agagagahha",this.props.allUsers)
-    const All=this.props.allUsers
-    const displayList=All.map(this.createTask)
+    const All = this.props.allUsers;
+    const displayList = All.map(this.createTask);
     return (
-        <div>
-            <h2>userlist</h2>
-            {displayList}
-        </div>
-    )
+      <div>
+        <h2>userlist</h2>
+        {displayList}
+      </div>
+    );
   }
 }
 
-const mapStateToProps = state => ({
-  allUsers: state.allUsers.allUsers
-})
+const mapStateToProps = (state) => ({
+  allUsers: state.allUsers.allUsers,
+});
 
-const mapDispatchToProps = dispatch => ({
-  FetchAllUsers: () => dispatch(FetchAllUsers())
-})
+const mapDispatchToProps = (dispatch) => ({
+  FetchAllUsers: () => dispatch(FetchAllUsers()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserList);
