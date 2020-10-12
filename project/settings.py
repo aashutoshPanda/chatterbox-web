b8e0e80619af598f7473f1cd03f4d1d49c5f22e5
aashutoshPanda <iamashutoshpanda@gmail.com>
2020-10-12 21:52:32 +0530

    templates mentioned in settings

--- project/settings.py
+++ project/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
