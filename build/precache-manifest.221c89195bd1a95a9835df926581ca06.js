f15a1d2b930d39e06f659113657a2e5b81569202
aashutoshPanda <iamashutoshpanda@gmail.com>
2020-10-12 21:05:02 +0530

    more changes after build

---
+++ build/precache-manifest.221c89195bd1a95a9835df926581ca06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0262a6aacb8dca44fabe3562d7183cc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fa21db3b973afa925c7",
+    "url": "/static/css/2.4190a901.chunk.css"
+  },
+  {
+    "revision": "440b685b6f8385c1ff88",
+    "url": "/static/css/main.814d807c.chunk.css"
+  },
+  {
+    "revision": "6fa21db3b973afa925c7",
+    "url": "/static/js/2.1c327810.chunk.js"
+  },
+  {
+    "revision": "d8d61e173bd8ea4b00f4947fffb7761f",
+    "url": "/static/js/2.1c327810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "440b685b6f8385c1ff88",
+    "url": "/static/js/main.d9cae446.chunk.js"
+  },
+  {
+    "revision": "e0d14143b24bf01dd1e2",
+    "url": "/static/js/runtime-main.33af6c64.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
