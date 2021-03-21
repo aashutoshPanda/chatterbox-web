5cad661dc0b1d27d9ade702857207c8e493d90d1
Ashutosh Panda <iamashutoshpanda@gmail.com>
2021-03-21 16:56:48 +0530

    update

--- project/secrets.py
+++ project/secrets.py
@@ -1,7 +1,7 @@
 def get_secret(key):
     secrets = {
-        "STREAM_API_KEY": "3fcmfmn49nnk",
-        "STREAM_API_SECRET": "zb4n643gsbmgubkw2ztq49nnhg94rakwhxv57xksrdt7acj37xz6metwn83adpr3",
+        "STREAM_API_KEY": "w3b3rqkg4jn6",
+        "STREAM_API_SECRET": "8qptcphs9jq8k35fajawuavn9bvt2mgf9f3vsxceev48s8xrnk24uekuzck3mgj9",
         "CLOUDINARY_CLOUD_NAME": "dmquemhgb",
         "CLOUDINARY_API_SECRET": "k5p1Y6NkUX0GKabqNR2MHl4vF-Y",
         "CLOUDINARY_API_KEY": "247387224716728"
